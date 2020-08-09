94da336fe52b87fc844c2573abc0acfbed24522d
kuldeep146 <kuldeepchotaliya17@gmail.com>
2020-08-09 15:13:46 +0530

    second commit

---
+++ my_portfolio/views.py
@@ -0,0 +1,25 @@
+from django.shortcuts import render,HttpResponse
+from datetime import datetime
+from my_portfolio.models import Contact
+from django.contrib import messages
+# Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def project(request):
+    return render(request,'project.html')
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        desc = request.POST.get('desc')
+        contact = Contact(name=name,email=email,desc=desc,date=datetime.today())
+        contact.save()
+        messages.success(request, 'Thanks for contacting me!')
+    return render(request,'contact.html')
+
+
